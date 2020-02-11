cf51e3f00f152138cda7491b874ca7be60b6f70a
seokgyo <seokgyo@gmail.com>
2020-02-12 01:00:42 +0900

    ping pong with worker.

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './lib/index.js',
+    worker: './lib/worker.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'js.fdp',
+      excludeChunks: ['worker'],
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
